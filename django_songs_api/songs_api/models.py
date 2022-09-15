246cc8222f187e64fcaf4078bf2e8dee9ac2e6ee
Julia Friend <juliafriend@Julias-MacBook-Pro.local>
2022-09-15 11:59:13 -0400

    added by workinggit add .git add .

--- django_songs_api/songs_api/models.py
+++ django_songs_api/songs_api/models.py
@@ -5,4 +5,5 @@ class Song(models.Model):
     title = models.CharField(max_length=32)
     artist = models.CharField(max_length=32)
     image = models.CharField(max_length=600)
+    added_by = models.CharField(max_length=32, default=True)
     listened_to = models.BooleanField(default=True)
