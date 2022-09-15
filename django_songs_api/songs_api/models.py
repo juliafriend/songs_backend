fb8fafe90472dfa321ad2f281557967160310a52
Julia Friend <juliafriend@Julias-MacBook-Pro.local>
2022-09-15 12:21:20 -0400

    deleted checkbox

--- django_songs_api/songs_api/models.py
+++ django_songs_api/songs_api/models.py
@@ -5,5 +5,4 @@ class Song(models.Model):
     title = models.CharField(max_length=32)
     artist = models.CharField(max_length=32)
     image = models.CharField(max_length=600)
-    added_by = models.CharField(max_length=32, default=True)
-    listened_to = models.BooleanField(default=True)
+    added_by = models.CharField(max_length=32)
